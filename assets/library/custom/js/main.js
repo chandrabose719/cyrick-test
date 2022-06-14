e0b8a21500290e6844ce9e11be27791de6443e00
chandrabose719 <chandrabose719@gmail.com>
2022-06-14 19:23:05 +0530

    about page added

--- assets/library/custom/js/main.js
+++ assets/library/custom/js/main.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
 
   // preload
   var preloader = document.getElementById("preloader");
-  window.addEventListener("load", function(){
-    preloader.style.display = "none";
-  });
+  if (preloader) {
+    window.addEventListener("load", function(){
+      preloader.style.display = "none";
+    });
+  }  
 
   // header on scroll
   const selectHeader = document.querySelector('#header');
@@ -16,6 +18,18 @@ $(document).ready(function(){
       window.scrollY > 300 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
     });
   }
+
+  // quick links on click
+  $('.scrollto').bind('click', function(event) {
+    var $anchor = $(this);
+    var nav = $($anchor.attr('href'));
+    if (nav.length) {
+      $('html, body').stop().animate({				
+        scrollTop: $($anchor.attr('href')).offset().top				
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    }
+  });
 
   // scroll to top  
   const scrollTop = document.querySelector('.scroll-top');
@@ -34,11 +48,11 @@ $(document).ready(function(){
 });
 function sendEmail() {
   Email.send({
-    Host: "smtp.gmail.com",
-    Username: "sender@email_address.com",
-    Password: "Enter your password",
-    To: 'receiver@email_address.com',
-    From: "sender@email_address.com",
+    Host: "smtp.elasticemail.com",
+    Username: "chandrabose719.videos@gmail.com",
+    Password: "9DA54DC90FFB2018B1F51AB896E6345C61D8",
+    To: 'chandrabose719@gmail.com',
+    From: "chandrabose719.videos@gmail.com",
     Subject: "Sending Email using javascript",
     Body: "Well that was easy!!",
   })
